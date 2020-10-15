57fa225ba6038b0ebb0839310c1e3135da679023
Jco Bea <jcobea@gmail.com>
2020-10-15 21:05:17 +0800

    Add news app xcode project

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Jco Bea on 10/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
